3505a984d2de653f383d4d92b37f7449952e42b1
Tomas Hermanek <tomas.hermanek@petitpress.sk>
2025-05-14 13:26:43 +0200

    wip

--- tests/config/services/services.php
+++ tests/config/services/services.php
@@ -18,14 +18,6 @@ return static function (ContainerConfigurator $configurator): void {
     $services->set('TestRedis', Redis::class)
         ->call('connect', [env('REDIS_HOST'), env('REDIS_PORT')->int()])
         ->call('select', [env('REDIS_DB')->int()])
-        ->call('setOption', [Redis::OPT_PREFIX, 'common_web_bundle_' . env('APP_ENV')])
+        ->call('setOption', [Redis::OPT_PREFIX, 'anzutap_bundle_' . env('APP_ENV')])
     ;
-
-//    $services
-//        ->load(
-//            namespace: 'Anzu\CommonWebBundle\Tests\Data\Prosemirror\Node\\',
-//            resource: __DIR__ . '/../../data/Prosemirror/Node/',
-//        )
-//        ->tag(AnzuCommonWebBundle::TAG_PROSEMIRROR_NODE)
-//    ;
 };
